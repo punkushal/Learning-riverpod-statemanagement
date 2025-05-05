e78feceab9381336899b3cbb00c9da092f9f07c4
punkushal <spycymagar@gmail.com>
2025-05-05 14:30:50 +0545

    State provider added

--- lib/providers/provider.dart
+++ lib/providers/provider.dart
@@ -1,5 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+//Simple example of basic provider to only read the value
 final nameprovider = Provider<String>((ref) {
   return 'Kushal Pun';
+});
+
+//Simple example of StateProvider to not only read it but also change the value
+//State provider is useful for switching dark/light theme
+final moodProvider = StateProvider((ref) {
+  return 'assets/smile.png';
 });
